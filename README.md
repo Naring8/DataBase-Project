e468b3b8f221097b67b530867daba21b0ed09994
Naring <skell01@naver.com>
2024-11-17 21:05:45 +0900

    Create Templates
    Templates(Main, Info, Create, Update, Delete)

---
+++ README.md
@@ -0,0 +1,4 @@
+# 2024 DB Project
+
+2024-11-17
+- Create `template` (before create db(database))
--- WebProject.py
+++ WebProject.py
@@ -2,3 +2,32 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
+
+@app.route('/')
+def mainPage():
+    # Main Web Page
+    return render_template('MainPage.html')
+
+@app.route('/ShowGamesInfo/')
+def showGamesInfo():
+    # connect to Games DB
+    return render_template('GamesInfo.html')
+
+@app.route('/InsertNewGame/')
+def newGame():
+    # Enroll New Game
+    return render_template('NewGamePage.html')
+
+@app.route('/UpdateGame/')
+def updateGame():
+    # Update Game
+    return render_template('UpdateGamePage.html')
+
+@app.route('/DeleteGame/')
+def deleteGame():
+    # Delete Game
+    return render_template('DeleteGamePage.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='127.0.0.1', port=7000)
