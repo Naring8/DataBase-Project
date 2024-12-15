1e46929add80fcc764183aaa47eae681eaf8e92b
Naring <skell01@naver.com>
2024-12-16 05:55:20 +0900

    Add Game Insertion Code
    Create a new tuple for game submission

--- WebProject.py
+++ WebProject.py
@@ -17,10 +17,35 @@ def showGamesInfo():
     db.close()
     return render_template('GamesInfo.html', games=items)
 
-@app.route('/InsertNewGame/')
+@app.route('/InsertNewGame/', methods=['GET', 'POST'])
 def newGame():
     # Enroll New Game
-    return render_template('NewGamePage.html')
+    platforms = ['3DS','PC', 'PS4', 'PS5', 'Switch', 'X360', 'XOne']
+    genres = ['Action', 'Adventure', 'Fighting', 'Misc', 'Platform', 'Puzzle', 'Racing', 'Role-Playing', 'Shooter', 'Simulation', 'Sports', 'Strategy']
+    if request.method == 'POST':
+        name = request.form.get('name')
+        platform = request.form.get('platform')
+        year = request.form.get('year')
+        genre = request.form.get('genre')
+        publisher = request.form.get('publisher')
+
+        if not name or not platform:
+            return "Error: Check name and platform input", 400
+        
+        try:
+            db = sqlite3.connect('games.db')
+            cursor = db.cursor()
+            cursor.execute(
+                'INSERT INTO Games (Name, Platform, Year, Genre, Publisher) VALUES (?, ?, ?, ?, ?)', 
+                (name, platform, year, genre, publisher))
+            db.commit()
+        except sqlite3.Error as e:
+            db.rollback()
+            return f"Databas error: {e}", 500
+        finally:
+            db.close()
+        return redirect('/ShowGamesInfo/')
+    return render_template('NewGamePage.html', platforms = platforms, genres = genres)
 
 @app.route('/UpdateGame/')
 def updateGame():
